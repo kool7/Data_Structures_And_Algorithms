6013b968de1cf235726c6ac9e4fc890a55f31603
kool7 <kschouhan714@gmail.com>
2020-11-02 20:57:14 +0530

    day24 remove duplicate from LL python

--- Hackerrank/30days/day24.py
+++ Hackerrank/30days/day24.py
@@ -1,47 +1,63 @@
+'''
+Program to remove duplicate from python linked list.
+'''
+
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
-        self.next = None 
-class Solution: 
-    def insert(self,head,data):
-            p = Node(data)           
-            if head==None:
-                head=p
-            elif head.next==None:
-                head.next=p
-            else:
-                start=head
-                while(start.next!=None):
-                    start=start.next
-                start.next=p
-            return head  
-    def display(self,head):
+        self.next = None
+
+class Solution:
+    def insert(self, head, data):
+        p = Node(data)
+        
+        if head is None:
+            head = p
+        elif head.next is None:
+            head.next = p
+        else:
+            start = head
+            while(start.next != None):
+                start = start.next
+            start.next = p
+        return head
+
+    def display(self, head):
         current = head
         while current:
-            print(current.data,end=' ')
+            print(current.data, end = ' ')
             current = current.next
 
-    def removeDuplicates(self,head):
-        #Write your code here
+    def removeDuplicate(self, head):
         if head is None:
             return
 
         start = head
-        while(start.next!=None):
+        while(start.next != None):
             if start.data == start.next.data:
                 start.next = start.next.next
             else:
                 start = start.next
-        return head       
-                
+        return head
 
-            
+# assigning solution class to an object
+mylinkedlist = Solution()
 
-mylist= Solution()
-T=int(input())
-head=None
+# number of times ask user to take input
+T = int(input())
+
+# head --> None
+head = None
+
+# looping for T num of times
 for i in range(T):
-    data=int(input())
-    head=mylist.insert(head,data)    
-head=mylist.removeDuplicates(head)
-mylist.display(head)
+    data = int(input())
+    head = mylinkedlist.insert(head, data)
+
+# removing duplicate elements from LL
+head = mylinkedlist.removeDuplicate(head)
+
+
+# display elemets in LL
+mylinkedlist.display(head)
+            

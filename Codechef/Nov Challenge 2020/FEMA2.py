374d3f66281f7aeb1fe6414302a3fb35881d0480
kool7 <kschouhan714@gmail.com>
2020-11-15 18:01:55 +0530

    FEMA2 Codechef Nov challenge 2020

--- Codechef/Nov Challenge 2020/FEMA2.py
+++ Codechef/Nov Challenge 2020/FEMA2.py
@@ -2,75 +2,71 @@ T = int(input())
 for i in range(T):
     N, K = map(int, input().split())
     S = input()
-
-    i = 0
-    j = 0
-    sheet = 0
+        
+    MAGNET = 0
+    IRON  = 0
     ANS = 0
+    SHEET = 0
   
-    while (i < N or j < N):
-        if i == S.find('M', i):
-            print(f'first ith loop {i}')
-            if j == S.find('I', j):
-                if (i > j):
-                    for k in range(j, i + 1):
-                        if k == S.find(':'):
-                            sheet += 1
+    while (MAGNET < N and IRON < N):
+    
+        if MAGNET == S.find('M', MAGNET):
+            
+            if IRON == S.find('I', IRON):
 
-                    P = K + 1 - j - 1 - sheet
-
+                if MAGNET > IRON:
+                    for i in range(IRON, MAGNET + 1):
+                        if i == S.find(':', i):
+                            SHEET += 1
+                    
+                    P = K + 1 - abs(IRON - MAGNET) - SHEET
+        
                     if P > 0:
                         ANS += 1
-                        i += 1
-                        j += 1
-                        sheet = 0
+                        MAGNET += 1
+                        IRON += 1
+                        SHEET = 0
 
-                    if P <= 0:
-                        if(i<j):
-                            i += 1
+                    else:
+                        if IRON < MAGNET:
+                            IRON += 1
                         else:
-                            j += 1
+                            MAGNET += 1
+                        
+                elif MAGNET < IRON:
 
-                elif (i < j):
-                    for k in range(i, j + 1):
-                        if k == S.find(':', k):
-                            sheet += 1
-                    print(sheet)
-                    P = K + 1 - j - 1 - sheet
+                    for i in range(MAGNET, IRON + 1):
+                        if i == S.find(':', i):
+                            SHEET += 1
+                    
+                    P = K + 1 - abs(IRON - MAGNET) - SHEET
                     
                     if P > 0:
                         ANS += 1
-                        i += 1
-                        j += 1
-                        sheet = 0
-                        print(ANS)
-                        print(f'i loop {i}')
-                        print(f'j loop {j}')
+                        MAGNET += 1
+                        IRON += 1
+                        SHEET = 0
 
-                    if P <= 0:
-                        if(i<j):
-                            i += 1
+                    else:
+                        if MAGNET < IRON:
+                            MAGNET += 1
                         else:
-                            j += 1
+                            IRON += 1
 
-                print(f'first jth loop {j}')
-
-            elif j == S.find('X', j):
-                i = j
-                j += 1
-                i += 1
-                print(f'second jth loop {j}')
+            elif IRON == S.find('X', IRON):
+                MAGNET = IRON
+                MAGNET += 1 
+                IRON += 1
+            
             else:
-                j += 1
-                print(f'third jth loop {j}')
-
-        elif i == S.find('X', i):
-            j = i
-            i += 1
-            j += 1
-            print(f'second ith loop {i}')
+                IRON += 1
+                
+        elif MAGNET == S.find('X', MAGNET):
+            IRON = MAGNET
+            MAGNET += 1 
+            IRON += 1
+            
         else:
-            i += 1
-            print(f'third ith loop {i}')
+            MAGNET += 1
 
     print(ANS)

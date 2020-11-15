40383cb700004ed090b3fdccf21db19787539da5
kool7 <kschouhan714@gmail.com>
2020-11-15 15:49:42 +0530

    FEMA2 Nov 2020 codechef

--- Codechef/Nov Challenge 2020/FEMA2.py
+++ Codechef/Nov Challenge 2020/FEMA2.py
@@ -1,33 +1,76 @@
-# def find(s, ch):
-#     return [i + 1 for i, ltr in enumerate(s) if ltr == ch]
-
-
 T = int(input())
 for i in range(T):
     N, K = map(int, input().split())
     S = input()
-    F = []
+
+    i = 0
+    j = 0
+    sheet = 0
     ANS = 0
-    if len(S) == N:
-        for i in range(N):
-            if S[i] == 'M':
-                F.append(i)
-                continue
-            elif S[i] == 'I':
-                F.append(i)
-                continue
-                P = K + 1 - F[1] - F[0]
-                if P > 0:
-                    ANS += 1
+  
+    while (i < N or j < N):
+        if i == S.find('M', i):
+            print(f'first ith loop {i}')
+            if j == S.find('I', j):
+                if (i > j):
+                    for k in range(j, i + 1):
+                        if k == S.find(':'):
+                            sheet += 1
 
-            elif S[i] == ':':
-                F.append(i)
-                continue
-            elif S[i] == 'X':
-                F.append(i)
-                break
+                    P = K + 1 - j - 1 - sheet
 
-            # P = K + 1 - F[0] - F[1] - F[3]
+                    if P > 0:
+                        ANS += 1
+                        i += 1
+                        j += 1
+                        sheet = 0
 
-        print(F)
-        print(ANS)
+                    if P <= 0:
+                        if(i<j):
+                            i += 1
+                        else:
+                            j += 1
+
+                elif (i < j):
+                    for k in range(i, j + 1):
+                        if k == S.find(':', k):
+                            sheet += 1
+                    print(sheet)
+                    P = K + 1 - j - 1 - sheet
+                    
+                    if P > 0:
+                        ANS += 1
+                        i += 1
+                        j += 1
+                        sheet = 0
+                        print(ANS)
+                        print(f'i loop {i}')
+                        print(f'j loop {j}')
+
+                    if P <= 0:
+                        if(i<j):
+                            i += 1
+                        else:
+                            j += 1
+
+                print(f'first jth loop {j}')
+
+            elif j == S.find('X', j):
+                i = j
+                j += 1
+                i += 1
+                print(f'second jth loop {j}')
+            else:
+                j += 1
+                print(f'third jth loop {j}')
+
+        elif i == S.find('X', i):
+            j = i
+            i += 1
+            j += 1
+            print(f'second ith loop {i}')
+        else:
+            i += 1
+            print(f'third ith loop {i}')
+
+    print(ANS)
